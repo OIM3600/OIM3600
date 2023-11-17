b67dbbe7e8ac7981f7ce7f6f567d27db734561c8
Zhi Li <lzblack@users.noreply.github.com>
2023-11-17 12:37:06 -0500

    Finish Flask session

--- helloflask/app.py
+++ helloflask/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request, url_for
+from weather import get_temp
 
 app = Flask(__name__)
 
@@ -16,6 +17,84 @@ def hello():
     else:
         name = "guest"
     return render_template("hello.html", username=name)
+
+
+@app.route("/ai")
+def talk_ai():
+    # form that allows user to enter text
+    # After pressing "enter" key, send HTTP request to an endpoint of AI API
+    # After getting response from the API
+    # display the response
+    pass
+
+
+# @app.route("/weather")
+# def weather():
+#     return render_template("weather-form.html")
+
+
+# @app.route("/temp")
+# def temp():
+#     if "city" in request.args:
+#         city = request.args["city"]
+#         city = city.replace(" ", "%20")
+#     else:
+#         city = "Wellesley"
+#     temp = get_temp(city)
+#     return f"The temperature of {city} is {temp}."
+
+
+@app.get("/weather")
+def weather_get():
+    return render_template("weather-form1.html")
+
+
+@app.post("/weather")
+def weather_post():
+    city = request.form.get("city")
+    # temp = get_temp(city.replace(" ", "%20"))
+    temp = 60
+    # return f"The temperature of {city} is {temp}."
+    return render_template("weather-result.html", city=city, temp=temp)
+
+
+"""
+404 Handling
+"""
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "Not found. 😭"
+
+
+"""
+Registration example
+"""
+students = {}
+courses = ["Python", "Web", "Cybersecurity", "Mobile"]
+
+
+@app.get("/register")
+def register_get():
+    return render_template("registration-form.html", courses=courses)
+
+
+@app.post("/register")
+def register_post():
+    # validate the input
+    name = request.form.get("name")
+    course = request.form.get("course")
+    if course not in courses:
+        return f"Hello, hacker. You cannot register this course!"
+    # register name and course to the dict
+    students[name] = course
+    return redirect("/students")
+
+
+@app.route("/students")
+def show_students():
+    return render_template("students.html", students=students)
 
 
 # Create another route("/double?number=3") so the response will be 6
---
+++ helloflask/weather.py
@@ -0,0 +1,43 @@
+import urllib.request
+import json
+import pprint
+from config import OPENWEATHER_APIKEY
+
+
+def get_weather_data(city_name, country_code, api_key):
+    """
+    Get weather data for a specified city and country using the OpenWeatherMap API.
+    """
+    api_url = f"http://api.openweathermap.org/data/2.5/weather?q={city_name},{country_code}&APPID={api_key}&units=imperial"
+    print(api_url)
+
+    with urllib.request.urlopen(api_url) as response:
+        response_text = response.read().decode("utf-8")
+        weather_data = json.loads(response_text)
+
+    return weather_data
+
+
+def get_temp(city):
+    """
+    Get the temperature from the weather data.
+    """
+    API_KEY = OPENWEATHER_APIKEY
+    country_code = "us"
+    weather_data = get_weather_data(city, country_code, API_KEY)
+
+    return weather_data["main"]["temp"]
+
+
+def main():
+    API_KEY = OPENWEATHER_APIKEY
+    city_name = "Wellesley"
+    country_code = "us"
+
+    weather_data = get_weather_data(city_name, country_code, API_KEY)
+    # pprint.pprint(weather_data)
+    # print(get_temp(weather_data))
+
+
+if __name__ == "__main__":
+    main()
