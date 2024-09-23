79c94507df53b9b318b473cd5eaead4b13bd4c97
Zhi Li <lzblack@users.noreply.github.com>
2024-09-23 10:48:29 -0400

    Update mario_inclass.py

--- s07/mario_inclass.py
+++ s07/mario_inclass.py
@@ -8,11 +8,56 @@
 
 # ask user to enter the number of bricks, then print the brick row
 
-n = input('Enter the size: ')
-n = int(n)
 
-for i in range(n):
-    print("#", end="")
+# # Get the size from user
+# while True:
+#     n = input("Enter the size: ")
+#     n = int(n)
+#     # if n > 0 and n <= 8:
+#     if 0 < n <= 8:
+#         break
+#     else:
+#         print("Try again.")
 
-print()
 
+# # Build a n x n wall
+# for i in range(n):  # repeat the following task n times
+#     # The following 3 lines are doing one job: creating one row of n bricks
+#     for j in range(n):
+#         print("🧱", end="")
+#     print()
+
+
+# Get the size from user
+def get_size():
+    """
+    Gets the size of the wall, returns the size
+    """
+    while True:
+        n = input("Enter the size: ")
+        n = int(n)
+        # if n > 0 and n <= 8:
+        if 0 < n <= 8:
+            return n
+        else:
+            print("Try again.")
+
+
+# Build a n x n wall
+def build_wall(n):
+    """
+    Prints a n x n wall.
+    """
+    for i in range(n):  # repeat the following task n times
+        # The following 3 lines are doing one job: creating one row of n bricks
+        for j in range(n):
+            print("🧱", end="")
+        print()
+
+
+def main():
+    size = get_size()
+    build_wall(size)
+
+
+main()
