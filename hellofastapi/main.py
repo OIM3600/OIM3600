ad5406194635188877e778aa90be709b4eb0606d
Zhi Li <lzblack@users.noreply.github.com>
2024-11-18 10:24:57 -0500

    Initilalize fastapi

--- hellofastapi/main.py
+++ hellofastapi/main.py
@@ -1,12 +1,56 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 import uvicorn
 
 app = FastAPI()
+templates = Jinja2Templates("templates")
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World", "data": [1, 2, 3, 4]}
+# @app.get("/")
+# async def root():
+#     return {"message": "Hello World", "data": [1, 2, 3, 4]}
+
+
+# @app.get("/", response_class=HTMLResponse)
+# def homepage():
+#     html_content = """
+#     <html>
+#     <head>
+#         <title>Hello</title>
+#     </head>
+#     <body>
+#         <h1>Hello, world!</h1>
+#         <p>I am excited to learn FastAPI to create web applications.</p>
+#     </body>
+#     </html>
+#     """
+#     return html_content
+
+
+@app.get("/", response_class=HTMLResponse)
+def homepage(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request},
+    )
+
+
+@app.get("/hi", response_class=HTMLResponse)
+def say_hi():
+    return "Hi, guest"
+
+
+@app.get("/search", response_class=HTMLResponse)
+def search(q="python"):
+    """Search for a term, just like google"""
+    return f"We've found these results for the term {q}:"
+
+
+@app.get("/ai", response_class=HTMLResponse)
+def talk_ai(q="python"):
+    """Search for a term, just like google"""
+    return f"blah bla bla"
 
 
 if __name__ == "__main__":
